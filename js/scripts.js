960162021f3c22b4eb88a84a3b6ecf3c381b7a43
wtfoxx <leticiagmonteiro@gmail.com>
2022-02-01 11:01:38 -0800

    Started app code from scratch, created array of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,35 @@
-alert('Hello world');
+pokemonList = [{
+  name: 'Bulbasaur',
+  height: 0.7,
+  weight: 6.9,
+  type: ['grass', 'poison']
+},
 
-let favoriteFood = 'Burgers';
-document.write(favoriteFood);
+{
+  name: 'Charmander',
+  height: 0.6,
+  weight: 8.5,
+  type: ['fire']
+},
+
+{
+  name: 'Squirtle',
+  height: 0.5,
+  weight: 9.0,
+  type: ['water']
+},
+
+{ 
+  name: 'Pikachu',
+  height: 0.4,
+  weight: 6.0,
+  type: ['electric']
+},
+
+{ 
+  name: 'Ditto',
+  height: 0.3,
+  weight: 4.0,
+  type: ['normal']
+}
+]
