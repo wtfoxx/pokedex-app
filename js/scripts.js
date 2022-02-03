890d8756d1973d82c3032aca283512c86b23131f
wtfoxx <leticiagmonteiro@gmail.com>
2022-02-03 15:57:08 -0800

    Created function to store pokemon array, created conditional to check if entry is object

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,63 @@
-let pokemonList = [{
-  name: 'Bulbasaur',
-  height: 0.7,
-  weight: 6.9,
-  type: ['grass', 'poison']
-},
+let pokemonRepository = (function () {
 
-{
-  name: 'Charmander',
-  height: 0.6,
-  weight: 8.5,
-  type: ['fire']
-},
+    let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass', 'poison']
+  },
 
-{
-  name: 'Squirtle',
-  height: 0.5,
-  weight: 9.0,
-  type: ['water']
-},
+  {
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: ['fire']
+  },
 
-{ 
-  name: 'Pikachu',
-  height: 0.4,
-  weight: 6.0,
-  type: ['electric']
-},
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9.0,
+    type: ['water']
+  },
 
-{ 
-  name: 'Ditto',
-  height: 0.3,
-  weight: 4.0,
-  type: ['normal']
-}
-]
+  { 
+    name: 'Pikachu',
+    height: 0.4,
+    weight: 6.0,
+    type: ['electric']
+  },
+
+  { 
+    name: 'Ditto',
+    height: 0.3,
+    weight: 4.0,
+    type: ['normal']
+  }];
+
+  function add(pokemon) { 
+    if (typeof(pokemon) === 'object') {
+    pokemonList.push(pokemon);
+    console.log(pokemon.name + ' was added to the Pokédex successfully!')
+    } else {
+      console.log('Entry was not valid, please try again.')
+    }
+  };
+
+  function getAll() {
+    return pokemonList;
+  };
+
+  return {
+    getAll: getAll,
+    add: add
+  };
 
 
+})();
 
-pokemonList.forEach(function(pokemon) {
+
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height >= 0.7) {
     document.write('<p>This is ' + pokemon.name + '! It is ' + pokemon.weight + ' meters tall, and weights ' + pokemon.height + 'kg. <b>Wow! That\'s big!</b></p>');
   } else {
