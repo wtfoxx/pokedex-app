134dbd07ac306af7ffe7da2df2e0052d4eeb2332
wtfoxx <leticiagmonteiro@gmail.com>
2022-02-07 13:06:18 -0800

    Added new functions and a button listener
    This commit adds a few comments to the JS file for better understanding and study purposes.
    Adds a new conditional to the add function
    Adds a new function that creates a list of buttons based on the pokemon objects. Created a event listener to the button so that it logs back the pokemon name to the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//pokemonRepository is a IIFE function wrapping the pokemonList information so that the list variables are not accessed globally
 let pokemonRepository = (function () {
 
     let pokemonList = [{
@@ -35,8 +36,16 @@ let pokemonRepository = (function () {
     type: ['normal']
   }];
 
+  //This function adds the pokemon to the pokemonList. You have to make it an object with assigned keys for it to work. 
+  //It'll log a response to whether it worked or not in the console.
   function add(pokemon) { 
-    if (typeof(pokemon) === 'object') {
+    if (
+    typeof(pokemon) === 'object' &&
+    'name' in pokemon &&
+    'height' in pokemon &&
+    'weight' in pokemon &&
+    'type' in pokemon
+    ) {
     pokemonList.push(pokemon);
     console.log(pokemon.name + ' was added to the Pokédex successfully!')
     } else {
@@ -44,23 +53,47 @@ let pokemonRepository = (function () {
     }
   };
 
+  //This function returns all of the pokemon objects.
   function getAll() {
     return pokemonList;
   };
 
+//This function will log the name of the pokemon in the console. It's attached to another function down there.
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  //Creates list items that are buttons inside a list for every existing and newly added pokemon.
+  function addListItem(pokemon){
+    let list = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    list.appendChild(listItem);
+    //This button event listener will log back the pokemon's name you clicked on the console
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+  };
+
+
+//This is VERY important, so that you can access functions you created in this IIFE everywhere in the code.
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 
 
 })();
 
+pokemonRepository.add({name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire']});  
+
+console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height >= 0.7) {
-    document.write('<p>This is ' + pokemon.name + '! It is ' + pokemon.weight + ' meters tall, and weights ' + pokemon.height + 'kg. <b>Wow! That\'s big!</b></p>');
-  } else {
-    document.write('<p>This is ' + pokemon.name + '! It is ' + pokemon.weight + ' meters tall, and weights ' + pokemon.height + 'kg.');
-  }
+  pokemonRepository.addListItem(pokemon);
 });
